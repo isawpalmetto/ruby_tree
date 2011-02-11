9a0f924677f16acc278ed058481f41745c0f30a2
James Rhein <jim.rhein@gmail.com>
2011-02-10 18:44:07 -0600

    added linkedLst file and tree file

---
+++ linkedList.rb
@@ -0,0 +1,16 @@
+class LinkedList
+
+
+
+	# subclass Node
+	# each  Node is an element of the linked list
+	class Node
+
+
+
+	end
+
+
+
+
+end
---
+++ tree.rb
@@ -0,0 +1,7 @@
+require 'linkedList'
+
+
+class Tree
+
+
+end
